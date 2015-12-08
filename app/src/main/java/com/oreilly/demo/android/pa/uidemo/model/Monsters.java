c271ecd145df41b7e3eb123c93400b41d21e0f52
David Saffo <dsaffo@bitbucket.com>
2015-12-08 12:02:07 -0600

    monsters fix

--- app/src/main/java/com/oreilly/demo/android/pa/uidemo/model/Monsters.java
+++ app/src/main/java/com/oreilly/demo/android/pa/uidemo/model/Monsters.java
@@ -14,7 +14,7 @@ public class Monsters {
 
     private final LinkedList<Monster> monsters = new LinkedList<>();
     private final List<Monster> safeMonsters = Collections.unmodifiableList(monsters);
-
+    Monster[][] monsterArray = new Monster[10][10];
     private MonstersChangeListener monstersChangeListener;
 
     /** @param l set the change listener. */
@@ -35,15 +35,45 @@ public class Monsters {
      * @param y dot vertical coordinate.
      * @param color dot color.
      */
-    public void addMonster(final float x, final float y, final int color) {
+    public void addMonster(final int x, final int y, final int color) {
+
+        monsterArray[x][y] = new Monster(x, y, color);
         monsters.add(new Monster(x, y, color));
         notifyListener();
     }
 
     /** Remove all dots. */
     public void clearMonsters() {
-        monsters.clear();
+        for(int i = 0; i<10; i++) {
+            for (int j = 0; j < 10; j++) {
+                monsterArray[i][j] = null;
+            }
+        }
         notifyListener();
+    }
+
+    public int checkState (int x, int y){
+        Monster temp = monsterArray[x][y];
+        int color = temp.getColor();
+        return color;
+    }
+
+    public void removeMonster (int x, int y){
+        monsterArray[x][y] = null;
+        notifyListener();
+    }
+
+    public void moveMonsters(final int currentX, final int currentY, final int newX, final int newY){
+        monsterArray[newX][newY] = monsterArray [currentX][currentY];
+        monsterArray[currentX][currentY] = null;
+        notifyListener();
+    }
+
+    public boolean spaceEmpty (final int x, final int y){
+        if (monsterArray[x][y] == null)
+            return true;
+        else
+            return false;
     }
 
     private void notifyListener() {
